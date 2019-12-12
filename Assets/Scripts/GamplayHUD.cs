b93fb2936e5c9bec5dc7ccc6dc818707263c9b58
iLoveYourFace <jackdunphy983@gmail.com>
2019-12-13 03:30:55 +1000

    Gameover message updated to show score

--- Assets/Scripts/GamplayHUD.cs
+++ Assets/Scripts/GamplayHUD.cs
@@ -6,22 +6,27 @@ using UnityEngine.UI;
 public class GamplayHUD : MonoBehaviour
 {
     public Text score;
+    private int scoreCount;
 
     public Text coinsCollected;
     public GameObject endScreen;
+    public Text endScreenText;
 
     public void UpdateScoreText(int newScore)
     {
+        scoreCount = newScore;
         score.text = newScore.ToString();
     }
     
     public void UpdateCoinText(int newCoin)
     {
-        coinsCollected.text = "Coins = " + newCoin.ToString();
+        coinsCollected.text = newCoin.ToString();
     }
 
     public void IsDead()
     {
+        endScreenText.text = "YOU SCORED " + scoreCount + " BEFORE DYING";
+        score.text = "";
         endScreen.SetActive(true);
     }
 }
