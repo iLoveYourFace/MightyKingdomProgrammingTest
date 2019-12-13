de03dfc236a91c16b904a964e365ce84a75f6e17
iLoveYourFace <jackdunphy983@gmail.com>
2019-12-13 16:20:06 +1000

    grounded Check. Highscore and coin save. Improved HUD

--- Assets/Scripts/GamplayHUD.cs
+++ Assets/Scripts/GamplayHUD.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class GamplayHUD : MonoBehaviour
 {
-    public Text score;
+    public Text scoreText;
     private int scoreCount;
+    public Text highscoreText;
+    public GameObject newHighScoreUI;
+    
 
     public Text coinsCollected;
     public GameObject endScreen;
@@ -15,18 +18,37 @@ public class GamplayHUD : MonoBehaviour
     public void UpdateScoreText(int newScore)
     {
         scoreCount = newScore;
-        score.text = newScore.ToString();
+        scoreText.text = newScore.ToString();
     }
     
     public void UpdateCoinText(int newCoin)
     {
+        PlayerPrefs.SetInt("coinsPickedUp", newCoin);
         coinsCollected.text = newCoin.ToString();
     }
 
     public void IsDead()
     {
-        endScreenText.text = "YOU SCORED " + scoreCount + " BEFORE DYING";
-        score.text = "";
+        endScreenText.text = "YOU SCORED\n" + scoreCount + "\nBEFORE DYING";
+        scoreText.text = "";
         endScreen.SetActive(true);
+    }
+
+    public void UpdateHighscore()
+    {
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            highscoreText.text = PlayerPrefs.GetInt("highScore").ToString();
+        }
+    }
+
+    public void UpdateHighScoreUI(int highscore)
+    {
+        highscoreText.text = highscore.ToString();
+    }
+
+    public void EnableNewHighScoreUI()
+    {
+        newHighScoreUI.SetActive(true);
     }
 }
--- Assets/Scripts/ScoreTracking.cs
+++ Assets/Scripts/ScoreTracking.cs
@@ -7,23 +7,39 @@ public class ScoreTracking : MonoBehaviour
     public int coinsPickedUp;
     private GamplayHUD _gamplayHud;
     public bool isDead = false;
+    public int highScore;
 
     public int score;
     void Start()
     {
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            highScore = PlayerPrefs.GetInt("highScore");
+        }
+        
         _gamplayHud = FindObjectOfType<GamplayHUD>();
+        _gamplayHud.UpdateHighScoreUI(highScore);
+        
+        if (PlayerPrefs.HasKey("coinsPickedUp"))
+        {
+            coinsPickedUp = PlayerPrefs.GetInt("coinsPickedUp");
+        }
+        
+        _gamplayHud.UpdateCoinText(coinsPickedUp);
         StartCoroutine(AddToScoreEverySecond());
     }
 
     public void AddToScore(int scoreToAdd)
     {
         score += scoreToAdd;
+        CheckIfNewHighScore();
         _gamplayHud.UpdateScoreText(score);
     }
 
     public void AddToScore()
     {
         score += 100;
+        CheckIfNewHighScore();
         _gamplayHud.UpdateScoreText(score);
         StartCoroutine(AddToScoreEverySecond());
     }
@@ -41,5 +57,15 @@ public class ScoreTracking : MonoBehaviour
     {
         coinsPickedUp += coinsAdded;
         _gamplayHud.UpdateCoinText(coinsPickedUp);
+    }
+
+    public void CheckIfNewHighScore()
+    {
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("highScore", highScore);
+            _gamplayHud.EnableNewHighScoreUI();
+        }
     }
 }
