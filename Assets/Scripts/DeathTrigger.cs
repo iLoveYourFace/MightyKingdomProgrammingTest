335367d4e62b7d2cb7424be90c0c07de287bfed9
iLoveYourFace <jackdunphy983@gmail.com>
2019-12-15 03:02:23 +1000

    Pause feature at start and end of game

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -8,12 +8,14 @@ public class DeathTrigger : MonoBehaviour
     private GamplayHUD _gamplayHud;
     private ScoreTracking _scoreTracking;
     private SoundManager _soundManager;
+    private TimeScaleZero _timeScaleZero;
 
     private void Awake()
     {
         _soundManager = FindObjectOfType<SoundManager>();
         _gamplayHud = FindObjectOfType<GamplayHUD>();
         _scoreTracking = FindObjectOfType<ScoreTracking>();
+        _timeScaleZero = FindObjectOfType<TimeScaleZero>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,6 +27,7 @@ public class DeathTrigger : MonoBehaviour
             _soundManager.Play("DeadTheme");
             Debug.Log("Player died");
             _gamplayHud.IsDead();
+            _timeScaleZero.SetTimeScaleZero();
             _scoreTracking.isDead = true;
             other.gameObject.SetActive(false);
         }
---
+++ Assets/Scripts/TimeScaleZero.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaleZero : MonoBehaviour
+{
+    private bool timeStopped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTimeScaleZero();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (timeStopped)
+            {
+                SetTimeScaleOne();
+            }
+            else
+            {
+                SetTimeScaleZero();
+            }
+        }
+    }
+
+    public void SetTimeScaleZero()
+    {
+        Time.timeScale = 0;
+        timeStopped = true;
+    }
+
+    public void SetTimeScaleOne()
+    {
+        Time.timeScale = 1;
+        timeStopped = false;
+    }
+}
